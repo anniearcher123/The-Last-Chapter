fdab3ab4e27d644b338b105b0ed6cd2b82252edf
Annie Archer <107079254+anniearcher123@users.noreply.github.com>
2023-05-07 17:45:51 -0500

    Remove About link until fixed

--- tbr-react-project/src/containers/header.js
+++ tbr-react-project/src/containers/header.js
@@ -11,9 +11,9 @@ const Header = () => {
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Home</Link>
                     </li>
-                    <li className="nav-item">
+                    {/* <li className="nav-item">
                         <a className="nav-link" href="#">About</a>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
                         <Link className="nav-link" to="/discover">Discover</Link>
                     </li>
